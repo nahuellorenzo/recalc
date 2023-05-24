e2ae98e2cd0cb9f49fb2d986e8261638a5333f6a
Ulises Mijael Ben <ulisesmijaelben02@gmail.com>
2023-05-24 12:52:57 -0300

    Agrego test para comprobar que el atributo error se guarda en el history

--- test/models.test.js
+++ test/models.test.js
@@ -47,4 +47,25 @@ describe("History", () => {
         expect(histories[0].result).toEqual(-4)
         expect(histories[0].Operation.name).toEqual("SUB")
     })
+
+    test("Deberia poder guardar el atributo error en el history", async () => {
+        await createHistoryEntry({
+            firstArg: -2,
+            secondArg: 4,
+            result: -6,
+            operationName: "SUB",
+            error: null
+        })
+
+        const histories = await History.findAll({
+            include: [Operation]
+        })
+        
+        expect(histories.length).toEqual(1)
+        expect(histories[0].firstArg).toEqual(-2)
+        expect(histories[0].secondArg).toEqual(4)
+        expect(histories[0].result).toEqual(-6)
+        expect(histories[0].Operation.name).toEqual("SUB")
+        expect(histories[0].error).toEqual(null)
+    })
 })
