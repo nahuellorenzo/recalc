d01acb54f6405a18d252e7e0a7a1df9a0fcc9159
Manuel de la Mano <manudelamano@gmail.com>
2023-05-25 16:36:00 -0300

    Agrego test para endpoint de add

--- test/api.test.js
+++ test/api.test.js
@@ -10,7 +10,7 @@ describe("API substract", () => {
     test("Deberia responder con un 200 ok", async () => {
         const app = await api.build()
 
-        request(app).get('/api/v1/sub/2/1')
+        return request(app).get('/api/v1/sub/2/1')
             .expect(200)
             .expect('Content-Type', "application/json; charset=utf-8")
             .then((res) => {
@@ -18,3 +18,17 @@ describe("API substract", () => {
             })
     })
 })
+
+describe("API add", () => {
+    test("Si segundo parámetro es negativo, el resultado tiene que ser menor al primer parámetro y el endpoint devuelver un status 200.", async () => {
+        const app = await api.build()
+
+        return request(app).get('/api/v1/add/2/-1')
+            .expect(200)
+            .expect('Content-Type', "application/json; charset=utf-8")
+            .then((res) => {
+                expect(res.body.result).toBeLessThan(2);
+            })
+    })
+})
+
