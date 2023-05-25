6c7121bc3fa143a24714bf4dbd5778d344a21050
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-05-25 15:49:47 -0300

    Agrego test para el endpoint de multiplicacion

--- test/api.test.js
+++ test/api.test.js
@@ -10,11 +10,24 @@ describe("API substract", () => {
     test("Deberia responder con un 200 ok", async () => {
         const app = await api.build()
 
-        request(app).get('/api/v1/sub/2/1')
+        return request(app).get('/api/v1/sub/2/1')
             .expect(200)
             .expect('Content-Type', "application/json; charset=utf-8")
             .then((res) => {
                 expect(res.body.result).toEqual(1);
+            })
+    })
+})
+
+describe("API multiply", () => {
+    test("Deberia responder con un 200 ok", async () => {
+        const app = await api.build()
+
+        return request(app).get('/api/v1/mul/3.1/5.4')
+            .expect(200)
+            .expect('Content-Type', "application/json; charset=utf-8")
+            .then((res) => {
+                expect(res.body.result).toBeCloseTo(16.74);
             })
     })
 })
