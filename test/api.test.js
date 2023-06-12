8a8076258cbfbb7500df5088812a68ee3a17b68b
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2023-06-12 14:55:11 -0300

    Modifico test que elimina todo el Historico

--- test/api.test.js
+++ test/api.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const api = require('../src/api.js');
 const { seed } = require('../src/seed.js');
 const { getHistory } = require('../src/models.js');
+const { createHistoryEntry } = require('../src/models.js');
 
 beforeEach(async () => {
     await seed()
@@ -117,6 +118,18 @@ describe("API sqrt", () => {
 describe("History deleted All", () => {
     test("Deberia eliminar History con un status 200.", async () => {
         const app = await api.build()
+        await createHistoryEntry({
+            firstArg: 2,
+            secondArg: 2,
+            result: 0,
+            operationName: "SUB"
+        });
+        await createHistoryEntry({
+            firstArg: 2,
+            secondArg: 2,
+            result: 0,
+            operationName: "SUB"
+        })
         const cantidad = (await getHistory()).length
         return request(app).get('/api/v1/allDeleted')
             .expect(200)
