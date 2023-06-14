2b9a6e821bdda9fdb263764ed4a091c6267c7bfe
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-06-14 12:15:33 -0300

    Agrego test e2e para la funcion mul

--- test/ui.test.js
+++ test/ui.test.js
@@ -48,4 +48,37 @@ test.describe('test', () => {
     expect(historyEntry.secondArg).toEqual(9)
     expect(historyEntry.result).toEqual(70)
   });
+
+  test('Deberia poder realizar una multiplicacion', async ({ page }) => {
+    await page.goto('./');
+
+    await page.getByRole('button', { name: '1' }).click()
+    await page.getByRole('button', { name: '0' }).click()
+    await page.getByRole('button', { name: '*' }).click()
+    await page.getByRole('button', { name: '5' }).click()
+
+    const [response] = await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/api/v1/mul/')),
+      page.getByRole('button', { name: '=' }).click()
+    ]);
+
+    const { result } = await response.json();
+    expect(result).toBe(50);
+
+    await expect(page.getByTestId('display')).toHaveValue(/50/)
+
+    const operation = await Operation.findOne({
+      where: {
+        name: "MUL"
+      }
+    });
+
+    const historyEntry = await History.findOne({
+      where: { OperationId: operation.id }
+    })
+
+    expect(historyEntry.firstArg).toEqual(10)
+    expect(historyEntry.secondArg).toEqual(5)
+    expect(historyEntry.result).toEqual(50)
+  });
 })
