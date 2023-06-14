09c333db41de358e5fdd4d39ba8f62a317383eb7
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2023-06-14 11:27:03 -0300

    Agrego boton de Pow

--- src/public/index.js
+++ src/public/index.js
@@ -1,7 +1,7 @@
 const $display = document.querySelector('.display')
 const $buttons = document.querySelector('.buttons')
 
-const operations = ['-'];
+const operations = ['-', '^2'];
 
 let currentDisplay = "";
 let operation = null;
@@ -19,6 +19,10 @@ $buttons.addEventListener('click', async (e) => {
 
         if (operation === "-") {
             result = await calculateSub(firstArg, secondArg)
+        }
+
+        if (operation === "^2"){
+            result = await calculatePow(firstArg)
         }
 
         reset = true;
@@ -40,6 +44,13 @@ $buttons.addEventListener('click', async (e) => {
 
 async function calculateSub(firstArg, secondArg) {
     const resp = await fetch(`/api/v1/sub/${firstArg}/${secondArg}`)
+    const { result } = await resp.json();
+
+    return result;
+}
+
+async function calculatePow(firstArg) {
+    const resp = await fetch(`/api/v1/pow/${firstArg}`)
     const { result } = await resp.json();
 
     return result;
