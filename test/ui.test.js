bb2b34a52c771b1aabaddfd8a0b2fb0fb2517e89
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2023-06-14 11:41:33 -0300

    Agrego test e2e a la función Pow

--- test/ui.test.js
+++ test/ui.test.js
@@ -48,4 +48,34 @@ test.describe('test', () => {
     expect(historyEntry.secondArg).toEqual(9)
     expect(historyEntry.result).toEqual(70)
   });
+
+  test('Deberia poder realizar una potencia', async ({ page }) => {
+    await page.goto('./');
+
+    await page.getByRole('button', { name: '7' }).click()
+    await page.getByRole('button', { name: '^2' }).click()
+
+    const [response] = await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/api/v1/pow/')),
+      page.getByRole('button', { name: '=' }).click()
+    ]);
+
+    const { result } = await response.json();
+    expect(result).toBe(49);
+
+    await expect(page.getByTestId('display')).toHaveValue(/49/)
+
+    const operation = await Operation.findOne({
+      where: {
+        name: "POW"
+      }
+    });
+
+    const historyEntry = await History.findOne({
+      where: { OperationId: operation.id }
+    })
+
+    expect(historyEntry.firstArg).toEqual(7)
+    expect(historyEntry.result).toEqual(49)
+  });
 })
